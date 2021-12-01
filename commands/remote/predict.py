3557f5b2fb0340094eccea2479ca3508b15d5c35
Adrian Webb <adrian@webb.sh>
2021-12-01 06:11:01 -0500

    Adding predictor_field and target_field methods to the remote predict command.

--- commands/remote/predict.py
+++ commands/remote/predict.py
@@ -3,5 +3,12 @@ from systems.commands.index import Command
 
 class Predict(Command('remote.predict')):
 
+    def predictor_field(self):
+        return 'job_combined_text'
+
+    def target_field(self):
+        return 'job_telework_eligible'
+
+
     def exec(self):
         self.success("Predicting remote!")
