8f428d6b56e43cb4b971e1b632200de2bd066939
Adrian Webb <adrian@webb.sh>
2021-12-01 04:42:05 -0500

    Updating the remote_ai command mixin and adding an initial remote_ai_training command mixin.

--- commands/mixins/remote_ai.py
+++ commands/mixins/remote_ai.py
@@ -7,3 +7,10 @@ class RemoteAIMixin(CommandMixin('remote_ai')):
         if self.data_name:
             return self.get_instance(self._dataset, self.data_name).provider.load()
         return None
+
+
+    def get_model(self, name, model_params):
+        return self.get_provider('remote_ai_model',
+            self.model_provider_name,
+            name, model_params
+        )
