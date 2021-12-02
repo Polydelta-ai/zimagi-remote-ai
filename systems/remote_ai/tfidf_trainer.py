1599e3435b4a9fbb977156b6b20cbb3cf9aef647
Adrian Webb <adrian@webb.sh>
2021-12-02 12:14:37 -0500

    Refactoring the tfidf_trainer remote_ai system library.

--- systems/remote_ai/tfidf_trainer.py
+++ systems/remote_ai/tfidf_trainer.py
@@ -6,36 +6,51 @@ import pickle
 
 class TfidfTrainer(object):
 
-    def __init__(self, model):
-        self.model = model
+    def __init__(self, model_provider):
+        self.model_provider = model_provider
         self.load()
 
 
     def word_vectorizer_file(self, vectorizer_path):
-        return "{}_{}".format(vectorizer_path, 'word_vectorizer.pk')
+        return "{}_{}_{}".format(vectorizer_path, self.model_provider.name, 'word_vectorizer.pk')
 
     def char_vectorizer_file(self, vectorizer_path):
-        return "{}_{}".format(vectorizer_path, 'char_vectorizer.pk')
+        return "{}_{}_{}".format(vectorizer_path, self.model_provider.name, 'char_vectorizer.pk')
 
 
     def load(self):
-        with self.model._model_project as project:
-            vectorizer_path = project.path(self.model.model_id)
+        with self.model_provider.get_model_project() as project:
+            vectorizer_path = project.path(self.model_provider.instance.name)
+            word_file = self.word_vectorizer_file(vectorizer_path)
+            char_file = self.char_vectorizer_file(vectorizer_path)
 
-            if project.exists(self.word_vectorizer_file(vectorizer_path)):
-                self.load_vectorizer(vectorizer_path)
+            if project.exists(word_file):
+                with open(word_file, "rb") as file:
+                    self.word_vectorizer = pickle.load(file)
+
+                with open(char_file, "rb") as file:
+                    self.char_vectorizer = pickle.load(file)
             else:
-                self.build()
+                self.build_vectorizer()
+                self.save()
 
-    def build(self):
-        self.build_vectorizer()
-        self.save()
+    def save(self):
+        with self.model_provider.get_model_project() as project:
+            vectorizer_path = project.path(self.model_provider.instance.name)
 
-    def load_vectorizer(self, vectorizer_path):
-        with open(self.word_vectorizer_file(vectorizer_path), "rb") as file:
-            self.word_vectorizer = pickle.load(file)
-        with open(self.char_vectorizer_file(vectorizer_path), "rb") as file:
-            self.char_vectorizer = pickle.load(file)
+            with open(self.word_vectorizer_file(vectorizer_path), "wb") as file:
+                pickle.dump(self.word_vectorizer, file)
+
+            with open(self.char_vectorizer_file(vectorizer_path), "wb") as file:
+                pickle.dump(self.char_vectorizer, file)
+
+    def remove(self):
+        with self.model_provider.get_model_project() as project:
+            vectorizer_path = project.path(self.model_provider.instance.name)
+
+            project.remove(self.word_vectorizer_file(vectorizer_path))
+            project.remove(self.char_vectorizer_file(vectorizer_path))
+
 
     def build_vectorizer(self):
         self.word_vectorizer = TfidfVectorizer(
@@ -58,17 +73,6 @@ class TfidfTrainer(object):
             dtype = numpy.float32,
             max_features = 5000
         )
-
-    def save(self):
-        with self.model._model_project as project:
-            self.save_vectorizer(project.path(self.model.model_id))
-
-    def save_vectorizer(self, vectorizer_path):
-        with open(self.word_vectorizer_file(vectorizer_path), "wb") as file:
-            pickle.dump(self.word_vectorizer, file)
-        with open(self.char_vectorizer_file(vectorizer_path), "wb") as file:
-            pickle.dump(self.char_vectorizer, file)
-
 
     def fit(self, dataset):
         self.word_vectorizer.fit(dataset)
