1dea25f2e28419201a46a9071be4c885b1c9c4d6
Adrian Webb <adrian@webb.sh>
2021-12-02 12:19:19 -0500

    Adding an initial remote predict implementation and updating the command specifications with other changes.

--- commands/remote/predict.py
+++ commands/remote/predict.py
@@ -1,14 +1,41 @@
+from django.conf import settings
+
 from systems.commands.index import Command
+from systems.remote_ai.preprocessor import Preprocessor
 
 
 class Predict(Command('remote.predict')):
 
-    def predictor_field(self):
-        return 'job_combined_text'
-
-    def target_field(self):
-        return 'job_telework_eligible'
-
-
     def exec(self):
-        self.success("Predicting remote!")
+        model = self.remote_ai_model
+        record = self._remote_ai_prediction.create(None,
+            model = model,
+            job_text = self.job_text
+        )
+        record.prediction = model.provider.predict(
+            Preprocessor.run(self.job_text)
+        )
+        record.threshold_diff = (record.prediction - settings.REMOTE_AI_PREDICTION_THRESHOLD)
+        record.classification = self.classify_prediction(record.prediction)
+
+        record.save()
+        self.data("Prediction", record.prediction, 'prediction')
+        self.data("Classification", record.classification, 'classification')
+
+
+    def classify_prediction(self, prediction):
+        classification = None
+
+        if prediction >= settings.REMOTE_AI_PREDICTION_THRESHOLD:
+            if prediction >= settings.REMOTE_AI_PREDICTION_HIGH_CONFIDENCE:
+                classification = 'ELIGIBLE high confidence'
+            else:
+                classification = 'ELIGIBLE medium confidence'
+
+        elif 1 - prediction >= settings.REMOTE_AI_PREDICTION_THRESHOLD:
+            if 1 - prediction >= settings.REMOTE_AI_PREDICTION_HIGH_CONFIDENCE:
+                classification = 'INELIGIBLE high confidence'
+            else:
+                classification = 'INELIGIBLE medium confidence'
+
+        return classification
